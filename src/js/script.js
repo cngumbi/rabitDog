83769c886405d4a513c10daf7daead7648d854fa
cngumbi <cngumbi35@gmail.com>
2022-12-10 02:40:06 +0300

    finished the frontend design

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+//typing animation
+var typed = new Typed(".typing", {
+    strings: [
+        "internet connection",
+        "fiber connection",
+        "phone repair"
+    ],
+    typeSpeed: 90,
+    BackSpeed: 50,
+    loop: true
+})
--- src/js/style-switcher.js
+++ src/js/style-switcher.js
@@ -1,7 +1,7 @@
 /*-----------------------toggle switcher-------------------------------------------*/
-const styleSwitcherToggler = document.querySelector(".style-switcher-taggler");
-styleSwitcherToggler.addEventListener("click", () => {
-    document.querySelector(".style-switcher").classList.toggle("open");
+const styleSwitcherToggler = document.querySelector('.style-switcher-taggler');
+styleSwitcherToggler.addEventListener('click', () => {
+    document.querySelector('.style-switcher').classList.toggle('open');
 });
 
 window.addEventListener('scroll', () => {
@@ -9,3 +9,30 @@ window.addEventListener('scroll', () => {
         document.querySelector('.style-switcher').classList.remove('open');
     }
 });
+
+/*theme*/
+const alternateStyles = document.querySelectorAll('.alternate-style');
+function setActiveStyle(theme){
+    alternateStyles.forEach((style) => {
+        if(theme === style.getAttribute('title'))
+        {
+            style.removeAttribute('disabled');
+        }else{
+            style.setAttribute('disable', 'true');
+        }
+    })
+}
+//day night
+const dayNight = document.querySelector('.day-night');
+dayNight.addEventListener('click', ()=>{
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.taggle('dark');
+})
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark')){
+        dayNight.querySelector('i').classList.add('fa-sun');
+    }else{
+        dayNight.querySelector('i').classList.add('fa-moon');
+    }
+})
