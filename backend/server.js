04713f62751a740c5d419be0601c5c9e075a1c18
cngumbi <cngumbi35@gmail.com>
2023-05-12 14:30:43 +0300

    added onclick on the side  bar

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
-const db = require('./config/mongoosDB');
+const db = require("./config/mongoosDB");
 const ContactRoute = require("./routes/contactRoutes");
 const config = require("./config/config");
 const ServiceRoute = require("./routes/serviceRoute");
@@ -10,9 +10,7 @@ const UserRoute = require("./routes/userRoute");
 
 const app = express();
 
-//const PORT = process.env.PORT || 5000;
 
-//middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/users', UserRoute);
@@ -23,12 +21,14 @@ app.use(express.static(path.join(__dirname, "/../frontend")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/../frontend/index.html"));
 });
-//error handling
-//code to handle all errors in express instance
+//middleware
+//error handling code to handle all errors in express instance
 app.use((err, req, res, next) => {
   const status = err.name && err.name === "Validation Error";
   res.status(status).send({ message: err.message });
 });
+//-0000000000000000000000000000000000000000000000000000000000
+//-0000000000000000000000000000000000000000000000000000000000
 app.listen(config.PORT, () => {
   console.log(`server running on port ${config.PORT}`);
 });
