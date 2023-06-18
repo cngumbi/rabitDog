3a28e0d546efdcaaebbdc664a0d0ba75f5096e6d
cngumbi35 <cngumbi35@gmail.com>
2023-06-18 15:51:51 +0300

    completed the registration section

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,17 +1,19 @@
+import profileImg from '../assets/account.png';
+import { clearUser, getUserInfo } from "../localStorage";
+
 const Header = {
     vignette: ()=>{
-        //const headerLinkEls = document.querySelectorAll('.navbar__links');
-//
-        //headerLinkEls.forEach(headerLinkEl => {
-        //    headerLinkEl.addEventListener('click', function(){
-        //        if(document.querySelector('.active') !== null){
-        //            document.querySelector('.active').classList.remove('active');
-        //        }
-        //        this.classList.add('active');
-        //    });
-        //});
+        //profile image
+        const profileImage = document.getElementById('profileImage');
+        profileImage.src = profileImg;
+        //signout
+        document.getElementById('signout').addEventListener('click', ()=>{
+            clearUser();
+            document.location.hash = '/';
+        })
     },
     render: ()=>{
+        const name  = getUserInfo();
         return `
             <div class="nav container">
                 <!--logo-->
@@ -20,14 +22,35 @@ const Header = {
                 <input type="checkbox" name="" id="menu">
                 <label for="menu"><i class="bx bx-menu" id="menu-icon"></i></label>
                 <!--Nav List -->
-                <ul class="navbar">
-                    <li><a href="/#/home" class="navbar__links active">Home</a></li>
-                    <li><a href="/#/about" class="navbar__links">About Us</a></li>
-                    <li><a href="/#/services" class="navbar__links">Services</a></li>
-                    <li><a href="/#/properties" class="navbar__links">Properties</a></li>
-                </ul>
-                <!--Log in Button -->
-                <a href="/#/user-current" class="btn">Log In</a>
+                ${
+                    name ? `
+                    <ul class="navbar">
+                        <li><a href="/#/home" class="navbar__links active">Home</a></li>
+                        <li><a href="/#/about" class="navbar__links">About Us</a></li>
+                        <li><a href="/#/services" class="navbar__links">Services</a></li>
+                        <li><a href="/#/properties" class="navbar__links">Properties</a></li>
+                    </ul>
+                    <div class="online">
+                        <img class="nav-profile-img" id="profileImage">
+                    </div>
+                    <div class="user-info">
+                        <div>
+                            <a href="/#/profile"><h3>${name}</h3></a>
+                        </div>
+                    </div>
+                    <!--Log in Button -->
+                    <a href="" id="signout">Sign Out</a>
+                    ` :` 
+                    <ul class="navbar">
+                        <li><a href="/#/home" class="navbar__links active">Home</a></li>
+                        <li><a href="/#/about" class="navbar__links">About Us</a></li>
+                        <li><a href="/#/services" class="navbar__links">Services</a></li>
+                        <li><a href="/#/properties" class="navbar__links">Properties</a></li>
+                    </ul>                   
+                    <!--Log in Button -->
+                    <a href="/#/user-current" class="btn">Log In</a>
+                    `
+                }
             </div>
         `;
     }
