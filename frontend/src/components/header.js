b1c57c4ffd9ad735d8e29edcfd11e4fedbbcfde3
cngumbi <cngumbi35@gmaal.com>
2023-07-30 22:40:01 +0300

    started new changes #13

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -17,45 +17,22 @@ const Header = {
         
     },
     render: ()=>{
-        const {name}  = getUserInfo();
         return `
             <div class="nav container">
                 <!--logo-->
-                <a href="/" class="logo"><i class='bx bx-home'></i>Mwendya</a>
+                <a href="/" class="logo"><i class='bx bx-home'></i>Mwendya planners</a>
                 <!--Menu Icons-->
                 <input type="checkbox" name="" id="menu">
                 <label for="menu"><i class="bx bx-menu" id="menu-icon"></i></label>
                 <!--Nav List -->
-                ${
-                    name ? `
-                    <ul class="navbar">
-                        <li><a href="/#/home" class="navbar__links active">Home</a></li>
-                        <li><a href="/#/about" class="navbar__links">About Us</a></li>
-                        <li><a href="/#/services" class="navbar__links">Services</a></li>
-                        <li><a href="/#/properties" class="navbar__links">Properties</a></li>
-                    </ul>
-                    <div class="online">
-                        <img class="nav-profile-img" id="profileImage">
-                    </div>
-                    <div class="user-info">
-                        <div>
-                            <a href="/#/profile"><h3>${name}</h3></a>
-                        </div>
-                    </div>
-                    <!--Log in Button -->
-                    <button type="button" id="signout" class="btn">Sign Out</button>
-                    <!--<a href="" id="signout">Sign Out</a>-->
-                    ` :` 
-                    <ul class="navbar">
-                        <li><a href="/#/home" class="navbar__links active">Home</a></li>
-                        <li><a href="/#/about" class="navbar__links">About Us</a></li>
-                        <li><a href="/#/services" class="navbar__links">Services</a></li>
-                        <li><a href="/#/properties" class="navbar__links">Properties</a></li>
-                    </ul>                   
-                    <!--Log in Button -->
-                    <!--<a href="/#/user-current" class="btn">Log In</a>-->
-                    `
-                }
+                <ul class="navbar">
+                    <li><a href="/#/home" class="navbar__links active">Home</a></li>
+                    <li><a href="/#/about" class="navbar__links">About Us</a></li>
+                    <li><a href="/#/services" class="navbar__links">Services</a></li>
+                    <li><a href="/#/properties" class="navbar__links">Properties</a></li>
+                </ul>                   
+                <!--Log in Button -->
+                <a href="/#/user-current" class="btn">Log In</a>
             </div>
         `;
     }
