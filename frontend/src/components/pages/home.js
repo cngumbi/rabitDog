1f49072fa16fedb89b0d113c45c87e57c90eb438
cngumbi <cngumbi35@gmaal.com>
2023-08-20 12:21:27 +0300

    added lobotomized owl and corrected error on all the files

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -1,8 +1,15 @@
+import homeImg from '../../assets/tent2.jpg'
 const Home = {
-    vignette: ()=>{},
+    vignette: ()=>{
+        const homeImage = document.getElementById('homeimage');
+        homeImage.src = homeImg;
+    },
     render: ()=>{
         return`
             <section class="home container" id="home">
+                <div class="home-image">
+                    <img  alt="main page image" id="homeimage" class="home-img">
+                </div>
                 <div class="home-text">
                     <h1> Find Your Next <br>Perfect Place To <br>Rent Tents.</h1>
                    <!-- <a href="/#/new-user-create" class="btn">Join Us</a>-->
