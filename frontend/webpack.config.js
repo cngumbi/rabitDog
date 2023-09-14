cb0ec14d13306b2b515e549d533efe13f6dce904
cngumbi <cngumbi35@gmail.com>
2023-09-14 13:38:14 +0300

    new changes

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
@@ -14,10 +14,6 @@ module.exports = {
         filename: '[name].[contenthash].js',
         clean: true,
         assetModuleFilename: '[name][ext]'
-    },
-    target: 'node',
-    node: {
-        __dirname: true,
     },
     devtool: 'source-map',
     devServer: {
@@ -60,24 +56,8 @@ module.exports = {
             {
                 test: /\.(png|jpg|jpe?g|gif|svg|ico)$/i,
                 type: 'asset/resource',
-                //use:{
-                //    loader: 'url-loader',
-                //    options: {
-                //        limit: 8192,
-                //        name: '[path][name].[ext]',
-                //        outputPath: 'uploads'
-                //    },
-                //},
                 
             },
-            //{
-            //    test: /\.(png|jpg|jpe?g|gif|svg|ico)$/i,
-            //    include: path.resolve(__dirname, 'src'),
-            //    type: "assets/resource",
-            //      generator: {
-            //          filename: "assets/[name][ext]",
-            //      }
-            //}
         ],
     }, 
     plugins: [
@@ -86,7 +66,7 @@ module.exports = {
             filename: 'index.html',
             template: 'src/template.html',
         }),
-        new BundleAnalyzerPlugin(),
+        //new BundleAnalyzerPlugin(),
         //plugin to extract css to a separate file
         new MiniCssExtractPlugin({
             filename: 'kyalo.[contenthash].css',
