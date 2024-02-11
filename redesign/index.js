79dc45809d5ce42df838e1111f3e341156ac675b
cngumbi <cngumbi35@gmail.com>
2024-02-11 19:32:57 +0300

    new changes

--- redesign/index.js
+++ redesign/index.js
@@ -226,8 +226,192 @@ function max(...numbers){
 }
 console.log(max(...numbers));
 */
-function phi([n00, n01, n10, n11]){
+/*function phi([n00, n01, n10, n11]){
     return (n11 * n00 - n10 * n01) /
         Math.sqrt((n10 + n11) * (n00 + n01) *
                   (n01 + n11) * (n00 + n10));
 }
+*/
+/*
+//range function
+function range(start, end, steps = start < end ? 1 : -1){
+    let xy = [];
+    if (steps > 0){
+        for(let i = start; i <= end; i+=steps){
+            xy.push(i);
+        }
+    }else {
+        for(let i = start; i >= end; i+=steps){
+            xy.push(i);
+        }
+    }
+    return xy;
+}
+function sum(...numbers){
+    let sum = 0;
+    for(let i = 0; i < numbers.length; i++){
+        sum +=numbers[i]
+    }
+    return sum;
+}
+console.log(sum.apply(null,range(1, 10, 6))); // apply is used to pass the array as a function paramenter
+//console.log(range(10, 1, -1));
+*/
+/*
+//reversing number arguments and outpiting in an array
+
+function reverseArray(...numbers){
+    let revers = [];
+    for (let i = numbers.length -1; i >= 0; i--) {
+        const indexValue = numbers[i];
+        revers.push(indexValue);
+    }
+    return revers;
+}
+console.log(reverseArray(1,2,3,4,5,6,7,8,9));
+*/
+//the repeat function
+function repeat(n, action){
+    for (let i = 0; i < n; i++){
+        action(i);
+    }
+}
+/*let labels = [];
+repeat(5, i => {
+    labels.push(`Unit ${i + 1}`);
+});
+console.log(labels);*/
+/*function noisy(f){
+    return(...args) => {
+        console.log("calling with", args);
+        let result = f(...args);
+        console.log("called with", args, ", returned", result);
+        return result;
+    };
+}
+
+noisy(Math.min)(3, 2, 1);
+*/
+//test argorithm  to use in project
+//function unless(test, then){
+//    if(!test) then();
+//}
+//repeat(10, n=>{
+//    unless(n % 2 ==1, () => {
+//        console.log(n, "is even");
+//    });
+//});
+//function to filter an array and decides which elements passed the test
+//and creates a new array for the passed element
+/*function filter(array, test){
+    let passed = [];
+    for (let element of array){
+        if(test(element)){
+            passed.push(element);
+        }
+    }
+    return passed;
+}
+*/
+//class Rabbit{
+//    constructor(type){
+//        this.type = type;
+//    }
+//    speak(line){
+//        console.log(`The ${this.type} rabit says '${line}'`);
+//    }
+//}
+//
+//let killerRabbit = new Rabbit("killer");
+//let blackRabbit = new Rabbit("black");
+/*let ages = {
+    Boris: 39,
+    Liang: 22,
+    Julia: 62
+};
+
+console.log(`Julia is ${ages["Julia"]}`);
+*/
+/*
+//-----------------------------------------------------
+class MatrixIterator{
+    constructor(matrix){
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+    next(){
+        if (this.y == this.matrix.height) return { done: true};
+
+        let value = {
+            x: this.x,
+            y: this.y,
+            value: this.matrix.get(this.x, this.y)
+        };
+        this.x++;
+        if(this.x == this.matrix.width){
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done: false};
+    }
+}
+*/
+
+/*class Temperature{
+    constructor(celsius){
+        this.celsius = celsius;        
+    }
+    get fahrenheit(){
+        return this.celsius * 1.8 + 32;
+    }
+    set fahrenheit(value){
+        this.celsius = (value - 32) / 1.8;
+    }
+    static fromFahrenheit(value){
+        return new Temperature((value - 32) / 1.8);
+    }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+temp.fahrenheit = 86;
+console.log(temp.celsius);
+*/
+/*class Matrix{
+    constructor(width, height, element = (x, y) => undefined){
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++){
+            for (let x = 0; x < width; x++){
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+    get(x, y){
+        return this.content[y * this.width + x];
+    }
+    set(x, y, value){
+        this.content[y * this.width + x] = value;
+    }
+}
+
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x, y) => undefined){
+        super(size, size, (x, y) => {
+            if(x < y) return element(y, x);
+            else return element(x, y);
+        });
+    }
+    set(x, y, value){
+        super.set(x, y, value);
+        if(x != y){
+            super.set(y, x, value);
+        }
+    }
+}
+let matrix = new SymmetricMatrix(5, (x, y) => `${x},${y}`);
+console.log(matrix.get(2, 3));
+*/
