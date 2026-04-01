68891f7f2e50d3a5b345024fda6c0017d4e88f77
cngumbi <cngumbi35@gmail.com>
2026-04-01 19:05:52 +0300

    changed the page name

--- frontend/src/components/authentication/forget.js
+++ frontend/src/components/authentication/forget.js
@@ -1,2 +1,21 @@
-const Forget = {};
+const Forget = {
+    vignette: ()=>{},
+    render: ()=>{
+        return`
+         <section class="login container">
+                <div class="login-container">
+                    <h2> Forgot Password</h2>
+                    <p>Kindly input you Registered Email below</p>
+                    <!--login form-->
+                    <form action="" id="forgot-form">
+                        <span>Email</span>
+                        <input type="email" name="email" id="email" placeholder="yourmail@gmail.com" required>
+                        <input type="submit" value="Submit" class="button">
+                    </form>
+                </div>
+            </section>
+        `;
+    }
+
+};
 export default Forget;
