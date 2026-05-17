740f4903bdea9375b043b4d7ae2e9bdb89649222
cngumbi <cngumbi35@gmail.com>
2026-05-17 12:45:29 +0300

    added Data Validation

--- backend/middleware/validateData.js
+++ backend/middleware/validateData.js
@@ -1,6 +1,13 @@
 const Joi = require('joi');
 
-exports.ValidateData = Joi.object({
+const ValidateData = ()=>{
+    return Joi.object({
     email: Joi.string().min(6).max(60).email({ tlds: { allow: ['com', 'net', 'org'] } }).required(),
-    password: Joi.string().min(6).required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()-+]).{6,}$')),
+    password: Joi.string().min(6).required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()\\-+]).{6,}$')).messages({
+        'string.pattern.base': 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+        'string.min': 'Password must be at least 6 characters long'
+    }),
 });
+};
+
+module.exports = { ValidateData };
